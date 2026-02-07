04cb1cb41bc5f7dac8e30362cdc5adb7bdf1b908
Kunal Wankhade <kwakhade1999gmail.com>
2026-02-07 19:08:45 +0000

    sd

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -6,9 +6,46 @@ resource "aws_instance" "this" {
   key_name               = var.key_name
 
   user_data = var.user_data
-
+  iam_instance_profile = aws_iam_instance_profile.argocd_profile.name
+  
   tags = {
     Name = var.name
     Env  = var.env
   }
+}
+############################################
+# IAM Role for ArgoCD EC2 (EKS Access)
+############################################
+
+resource "aws_iam_role" "argocd_ec2_role" {
+  name = "${var.name}-argocd-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+############################################
+# Attach EKS permissions
+############################################
+
+resource "aws_iam_role_policy_attachment" "eks_access" {
+  role       = aws_iam_role.argocd_ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+############################################
+# Instance Profile
+############################################
+
+resource "aws_iam_instance_profile" "argocd_profile" {
+  name = "${var.name}-argocd-instance-profile"
+  role = aws_iam_role.argocd_ec2_role.name
 }
